4aa5c51b82349e8bd6a08906706c72b189c9b8c9
John Muchovej <ionlights@users.noreply.github.com>
2018-09-03 22:58:02 -0400

    attempting to enable autopush with ssh

--- sites/deploy.sh
+++ sites/deploy.sh
@@ -24,13 +24,16 @@ git config user.email "travis-ci@ucfsigai.org"
 git add .
 git commit -m "Deploying ${SHA} to GitHub Pages"
 
+cp ../../dsg.ucfsigai.org.enc .
 ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
-chmod 600 deploy_key
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in dsg.ucfsigai.org.enc -out dsg.ucfsigai.org -d
+chmod 600 dsg.ucfsigai.org
 eval `ssh-agent -s`
 ssh-add deploy_key
+
+rm dsg.ucfsigai.org dsg.ucfsigai.org.enc
 
 git push --force origin $TARGET_BRANCH
