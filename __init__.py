c1ed2f4698009b2e567a163ac4b9a437d1562988
John M <j@ionlights.com>
2020-05-18 20:52:09 -0400

    typo in global __init__.py

--- __init__.py
+++ __init__.py
@@ -7,7 +7,7 @@ ns = Collection("autobot")
 # Custom libraries for Meeting management
 ns.add_collection(semester)
 # ns.add_collection(solutionbook)
-ns.add_collection(papers)
+# ns.add_collection(papers)
 
 # Third-party / Custom APIs
 # ns.add_collection(hugo)
